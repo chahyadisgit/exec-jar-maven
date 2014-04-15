7f56f6d80b7a1e57d4d8ce56675dfecffdaeeaac
unknown <SXCHAH@IDJKLLAP138.aceins.com>
2014-04-15 14:32:37 +0700

    first commit

---
+++ StdTemplateJar/src/main/java/com/std/pack/App.java
@@ -0,0 +1,13 @@
+package com.std.pack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, This is first executable jar with maven support" );
+    }
+}
